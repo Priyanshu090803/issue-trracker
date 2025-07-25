08dec3a22253cdb76418e05317ea0805f581376d
Priyanshu090803 <priyanshu122a@gmail.com>
2025-07-25 11:43:46 +0530

    Added Inter-font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import { Inter } from "next/font/google";
 
+
+const inter = Inter({subsets:['latin'],variable:'--font-inter'})
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -26,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
         <main>
