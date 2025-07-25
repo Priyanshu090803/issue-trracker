a2a66aef1ba649e10244dfe569cfb2187452e5bd
Priyanshu090803 <priyanshu122a@gmail.com>
2025-07-25 11:34:04 +0530

    Build New-Issue page

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const NewIssuePage = () => {
+  return (
+    <div className=' h-screen w-full flex justify-center'>  
+        <div className=' border  h-fit py-10 px-24 shadow-xl rounded-2xl  shadow-neutral-300 flex  flex-col items-center gap-5'>        
+            <input type="text" placeholder='Title' className=' px-4 py-2 border border-neutral-300 text-neutral-700 text-sm focus:outline-1 focus: outline-gray-400 rounded-lg'/>
+            <textarea name="" placeholder='Description' id="" rows={4}  className=' px-4 w-full py-2 border border-neutral-300 text-neutral-700 text-sm focus:outline-1 focus: outline-gray-400 rounded-lg'/>
+            <Button className=' px-3 shadow-lg shadow-neutral-500 py-1 bg-neutral-800 hover:bg-black active:bg-black cursor-pointer'>Submit New Issue</Button>
+        </div>
+
+    </div>
+  )
+}
+
+export default NewIssuePage
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 const Issues = () => {
   return (
     <div className=' p-4'>
-      <Button className=''>Click</Button>
+      <Button className=' bg-blue-500 hover:bg-blue-700'><Link href={"/issues/new"}>New Issue</Link></Button>
     </div>  
   )
 }
