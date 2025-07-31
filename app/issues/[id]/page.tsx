ea074268a0856268d4f4edc6f10281589fcabab5
Priyanshu090803 <priyanshu122a@gmail.com>
2025-07-31 14:52:08 +0530

    Add markdown preview

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import StatusBadge from '@/components/StatusBadge'
 import { notFound } from 'next/navigation'
 import React from 'react'
 import { FaClockRotateLeft } from "react-icons/fa6";
+import ReactMarkdown from 'react-markdown';
 
 interface Props {
     params:{id:string}
@@ -22,8 +23,9 @@ const IssueDetails = async ({params}:Props) => {
         <StatusBadge status={findIssue.status}/>
         <span className=' text-sm text-neutral-700 underline flex  gap-1 items-center'><FaClockRotateLeft className=' text-xs'/>{findIssue.createdAt.toDateString()}</span>
         </div>
-       
-        <p className=' border w-fit px-2 rounded-lg py-2'>{findIssue.description}</p>
+        <div className=' prose   border w-fit px-4 rounded-lg border-neutral-300 py-1'>
+        <ReactMarkdown >{findIssue.description}</ReactMarkdown>
+        </div>
     </div>
   )
 }
